8d47df1adcd76017a9af03a2cfd9704ade4e6ea5
Manis Thapa <thapa.manish27@gmail.com>
2024-08-22 16:52:00 +0530

    Update content

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -12,19 +12,37 @@ export const About = (props) => {
           <div className="col-xs-12 col-md-6">
             <div className="about-text">
               <h2>About Us</h2>
-              <p>{props.data ? props.data.paragraph : "loading..."}</p>
+              <p>{props.data ? 
+              <>
+              <b>Mission </b>
+                {props.data.mission}
+                <br></br>
+                <br></br>
+                <b>Story </b>
+                {props.data.story}
+                <br></br>
+                <br></br>
+              <b>Values</b><br></br>
+              {
+              props.data.ourvalues ? props.data.ourvalues.map((v,i)=>(
+                <><b>{v.split(":")[0]}</b>{v.split(":")[1]}<br></br></>
+              )) :""
+              }
+              </>
+              
+               : "loading..."} </p>
               <h3>Why Choose Us?</h3>
               <div className="list-style">
-                <div className="col-lg-6 col-sm-6 col-xs-12">
+                <div className="col-lg-12 col-sm-12 col-xs-12">
                   <ul>
                     {props.data
                       ? props.data.Why.map((d, i) => (
-                          <li key={`${d}-${i}`}>{d}</li>
+                          <li key={`${d}-${i}`}><b>{d.split(":")[0]}</b>{d.split(":")[1]}</li>
                         ))
                       : "loading"}
                   </ul>
                 </div>
-                <div className="col-lg-6 col-sm-6 col-xs-12">
+                {/* <div className="col-lg-6 col-sm-6 col-xs-12">
                   <ul>
                     {props.data
                       ? props.data.Why2.map((d, i) => (
@@ -32,7 +50,7 @@ export const About = (props) => {
                         ))
                       : "loading"}
                   </ul>
-                </div>
+                </div> */}
               </div>
             </div>
           </div>
